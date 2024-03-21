b396f298fdd585bfe0ae00c15b4458c3ff4ccf20
Stefano Casasola <ste.casasola@gmail.com>
2024-03-21 12:00:32 +0100

    adds more tests for HiddenSection component

--- src/__tests__/HiddenSection.test.jsx
+++ src/__tests__/HiddenSection.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import HiddenSection from '../components/HiddenSection'
 
 // to-do list dei nostri test
@@ -31,16 +31,59 @@ describe('Just button testing', () => {
   })
 
   // - il bottone all'avvio abbia contenuto "MOSTRA CARD"
-  //   it()
+  it('contains "MOSTRA CARD" initially', () => {
+    // 1)
+    render(<HiddenSection />)
+    // 2)
+    const button = screen.getByText(/mostra card/i)
+    // 3)
+    // 4)
+    expect(button).toBeInTheDocument()
+  })
 })
 
 describe('Other functionalities', () => {
   // - la card all'avvio NON ci deve essere
-  //   it()
+  it("checks the card's absence at launch", () => {
+    // 1)
+    render(<HiddenSection />)
+    // 2)
+    const img = screen.queryByRole('img') // questa riga dovrebbe tornare NULL
+    console.log('IMG', img)
+    // 3)
+    // 4)
+    // expect(img).not.toBeInTheDocument()
+    // expect(img).toBeFalsy()
+    expect(img).toBeNull()
+  })
   // - premendo una volta il bottone, la card deve comparire
-  //   it()
+  it('renders the card after clicking the button', () => {
+    // 1)
+    render(<HiddenSection />)
+    // 2)
+    const button = screen.getByText(/mostra card/i)
+    // 3)
+    fireEvent.click(button) // clicco una volta il bottone
+    // 4)
+    const img = screen.getByRole('img') // ora l'immagine dovrebbe esserci!
+    expect(img).toBeInTheDocument()
+  })
   // - sempre dopo aver premuto una volta il bottone, il testo del bottone deve diventare "NASCONDI CARD"
-  //   it()
+  it("changes the button's label after a single click on the button", () => {
+    render(<HiddenSection />)
+    const button = screen.getByText(/mostra card/i)
+    fireEvent.click(button)
+    // fireEvent.click(button) // se clicco due volte, l'etichetta del bottone torna ad essere "MOSTRA CARD" !
+    const differentLabelButton = screen.getByText(/nascondi card/i)
+    expect(differentLabelButton).toBeInTheDocument()
+  })
   // - cliccando DUE volte il bottone, non ci deve essere più la card, il bottone deve contenere "MOSTRA CARD"
-  //   it()
+  it("after two clicks of the button, the button's label resets", () => {
+    render(<HiddenSection />)
+    const button = screen.getByText(/mostra card/i)
+    fireEvent.click(button)
+    fireEvent.click(button)
+    const differentLabelButton = screen.queryByText(/nascondi card/i)
+    expect(differentLabelButton).not.toBeInTheDocument()
+  })
 })
